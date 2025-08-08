4bcdacdb80af6029806dfb25a4494ba746fb5e53
vclasicmouaz-max <vclasicmouaz@gmail.com>
2025-08-08 11:38:10 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     siteName: 'DoZOOM',
     images: [
       {
-        url: '/dozoom-logo-cinematic.png',
+        url: '/',
         width: 1200,
         height: 630,
         alt: 'DoZOOM Logo',
